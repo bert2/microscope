3e4099be2191945361f885941d6a2703c6c42e62
bert2 <shuairan@gmail.com>
2020-08-10 21:52:34 +0200

    add extension package

---
+++ src/Microscope.VSExtension/MicroscopePackage.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+namespace Microscope.VSExtension {
+    using System;
+    using System.Runtime.InteropServices;
+
+    using Microsoft.VisualStudio.Shell;
+
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#100", "#101", "ede7ef0f-017b-4b33-b7b3-c7480d3cc712", IconResourceID = 400)]
+    [Guid(PackageGuidString)]
+    public sealed class MicroscopePackage : AsyncPackage {
+        public const string PackageGuidString = "b798b46c-201a-470c-9e3e-fa0abb23dfa7";
+    }
+}

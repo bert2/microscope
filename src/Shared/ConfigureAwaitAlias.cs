74f53df6ba661077a839f8a66bfa529f297fa6e7
bert2 <shuairan@gmail.com>
2020-09-11 11:18:43 +0200

    add comment

--- src/Shared/ConfigureAwaitAlias.cs
+++ src/Shared/ConfigureAwaitAlias.cs
@@ -3,7 +3,10 @@
     using System.Threading.Tasks;
 
     public static class ConfigureAwaitAlias {
+        /// <summary>Alias for `ConfigureAwait(false)`.</summary>
         public static ConfiguredTaskAwaitable Caf(this Task t) => t.ConfigureAwait(false);
+
+        /// <summary>Alias for `ConfigureAwait(false)`.</summary>
         public static ConfiguredTaskAwaitable<T> Caf<T>(this Task<T> t) => t.ConfigureAwait(false);
     }
 }

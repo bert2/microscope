d31cee53d31141e7a20e6199b96607f8a6291fa7
bert2 <shuairan@gmail.com>
2020-09-14 20:21:18 +0200

    await refresh of remote CodeLens and log any errors

--- src/VSExtension/RefreshCommand.cs
+++ src/VSExtension/RefreshCommand.cs
@@ -4,8 +4,12 @@ namespace Microscope.VSExtension {
     using System;
     using System.ComponentModel.Design;
 
+    using Microscope.Shared;
+
     using Microsoft;
     using Microsoft.VisualStudio.Shell;
+
+    using static Microscope.Shared.Logging;
 
     using Task = System.Threading.Tasks.Task;
 
@@ -17,17 +21,29 @@ namespace Microscope.VSExtension {
             Assumes.Present(cmdService);
 
             var cmdId = new CommandID(PackageGuids.CmdSetId, PackageIds.RefreshCommandId);
-            var cmd = new OleMenuCommand((_, e) => Execute(refreshCodeLens, (OleMenuCmdEventArgs)e), cmdId);
+            var cmd = new OleMenuCommand(
+                (_, e) => ThreadHelper.JoinableTaskFactory.RunAsync(async delegate {
+                    await Execute(refreshCodeLens, (OleMenuCmdEventArgs)e).Caf();
+                }),
+                cmdId);
             cmdService.AddCommand(cmd);
         }
 
-        private static void Execute(Func<Guid, Task> refreshCodeLens, OleMenuCmdEventArgs e) {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            var arg = e.InValue as string
-                ?? throw new InvalidOperationException("RefreshCommand requires an argument.");
-            if (!Guid.TryParse(arg, out var dataPointId))
-                throw new InvalidOperationException("RefreshCommand requires an argument of type Guid.");
-            _ = refreshCodeLens(dataPointId);
+        private static async Task Execute(Func<Guid, Task> refreshCodeLens, OleMenuCmdEventArgs e) {
+            try {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                var arg = e.InValue as string
+                    ?? throw new InvalidOperationException("RefreshCommand requires an argument.");
+
+                if (!Guid.TryParse(arg, out var dataPointId))
+                    throw new InvalidOperationException("RefreshCommand requires an argument of type Guid.");
+
+                await refreshCodeLens(dataPointId).Caf();
+            } catch (Exception ex) {
+                Log(ex);
+                throw;
+            }
         }
     }
 }

3da05dd879bb0f3d86db1bef2cc752e670fe97f2
bert2 <shuairan@gmail.com>
2020-09-15 19:32:25 +0200

    use different pipe for VS experimental instance

--- src/Shared/Constants.cs
+++ src/Shared/Constants.cs
@@ -2,6 +2,13 @@
 
 namespace Microscope.Shared {
     public static class Constants {
-        public const string MicroscopePipe = "microscope-vs-to-codelens";
+        // We need to use a different pipe for the VS experimental instance.
+        // Otherwise both VS instances will compete for connecting CodeLenses.
+        public const string MicroscopePipe =
+#if DEBUG
+            @"microscope\exp\vs-to-codelens";
+#else
+            @"microscope\vs-to-codelens";
+#endif
     }
 }

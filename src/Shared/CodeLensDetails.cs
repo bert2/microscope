8a363f4acb0f9dc216dedc145ec573cf66c77e35
bert2 <shuairan@gmail.com>
2020-10-07 19:22:13 +0200

    fix typo

--- src/Shared/CodeLensDetails.cs
+++ src/Shared/CodeLensDetails.cs
@@ -3,9 +3,8 @@
 namespace Microscope.Shared {
     using System;
 
-    // Only holds the ID the `CodeLensDataPoint` for which the details have been requested.
-    // The actual instructions to display are stored in the VS process and can be accessed
-    // using the ID.
+    // Only holds the ID of the `CodeLensDataPoint` for which the details have been requested. The
+    // actual instructions to display are stored in the VS process and can be accessed using the ID.
     public class CodeLensDetails {
         public Guid DataPointId { get; set; }
         public CodeLensDetails(Guid dataPointId) => DataPointId = dataPointId;

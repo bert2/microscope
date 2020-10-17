2c77c84364ea3921379cf803ef6ff3732cf44c3b
bert2 <shuairan@gmail.com>
2020-10-17 11:53:02 +0200

    properly await refresh call

--- src/VSExtension/SaveCommandHandler.cs
+++ src/VSExtension/SaveCommandHandler.cs
@@ -1,15 +1,19 @@
 ﻿#nullable enable
 
 namespace CodeCleanupOnSave {
+    using System;
     using System.ComponentModel.Composition;
 
     using Microscope.VSExtension;
     using Microscope.VSExtension.Options;
 
     using Microsoft.VisualStudio.Commanding;
+    using Microsoft.VisualStudio.Shell;
     using Microsoft.VisualStudio.Text.Editor;
     using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
     using Microsoft.VisualStudio.Utilities;
+
+    using static Microscope.Shared.Logging;
 
     [Export(typeof(ICommandHandler))]
     [Name(nameof(SaveCommandHandler))]
@@ -18,15 +22,22 @@ namespace CodeCleanupOnSave {
     public class SaveCommandHandler : ICommandHandler<SaveCommandArgs> {
         public string DisplayName => nameof(SaveCommandHandler);
 
-        public bool ExecuteCommand(SaveCommandArgs args, CommandExecutionContext executionContext) {
-            var opts = GeneralOptions.Instance;
+        public bool ExecuteCommand(SaveCommandArgs args, CommandExecutionContext ctx) {
+            try {
+                var opts = GeneralOptions.Instance;
 
-            if (opts.Enabled && opts.RefreshOnSave) {
-                // CodeLenses usually only live as long as the document is open so we just refresh all the connected ones.
-                _ = CodeLensConnectionHandler.RefreshAllCodeLensDataPoints();
+                if (opts.Enabled && opts.RefreshOnSave) {
+                    _ = ThreadHelper.JoinableTaskFactory.RunAsync(async delegate {
+                        // CodeLenses usually only live as long as the document is open so we just refresh all the connected ones.
+                        await CodeLensConnectionHandler.RefreshAllCodeLensDataPoints();
+                    });
+                }
+
+                return true;
+            } catch (Exception ex) {
+                LogVS(ex);
+                throw;
             }
-
-            return true;
         }
 
         public CommandState GetCommandState(SaveCommandArgs args) => CommandState.Available;

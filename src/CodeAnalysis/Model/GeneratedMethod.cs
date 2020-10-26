89023f8f69fc9d4913c988592049ae3351664e32
bert2 <shuairan@gmail.com>
2020-10-26 10:13:39 +0100

    show return type and parameters of method; show full name as tooltip

--- src/CodeAnalysis/Model/GeneratedMethod.cs
+++ src/CodeAnalysis/Model/GeneratedMethod.cs
@@ -9,17 +9,34 @@ namespace Microscope.CodeAnalysis.Model {
     public readonly struct GeneratedMethod {
         public string Name { get; }
 
+        public string FullName { get; }
+
+        public string ReturnTypeName { get; }
+
+        public string ParameterList { get; }
+
         public IReadOnlyList<InstructionData> Instructions { get; }
 
-        public GeneratedMethod(string name, IReadOnlyList<InstructionData> instructions) {
+        public GeneratedMethod(string name, string fullName, string returnTypeName, string parameterList, IReadOnlyList<InstructionData> instructions) {
             Name = name;
+            FullName = fullName;
+            ReturnTypeName = returnTypeName;
+            ParameterList = parameterList;
             Instructions = instructions;
         }
 
         public static GeneratedMethod From(MethodDefinition method) => new GeneratedMethod(
             method.Name,
+            method.FullName,
+            method.ReturnType.FullName,
+            GetParameterList(method),
             method.Body.Instructions.Select(InstructionData.From).ToArray());
 
         public override string ToString() => Name;
+
+        private static string GetParameterList(MethodDefinition m) {
+            var parameterListStart = m.FullName.IndexOf('(');
+            return m.FullName.Substring(parameterListStart);
+        }
     }
 }

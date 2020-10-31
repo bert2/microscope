628c9641da44c3c762fdd7358619304d775d349b
bert2 <shuairan@gmail.com>
2020-10-31 14:37:17 +0100

    update nuke again and fix breaking change

--- build/Build.cs
+++ build/Build.cs
@@ -24,7 +24,7 @@ class Build : NukeBuild {
     public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Optional (default: 'Release')")]
-    readonly Configuration Configuration = Configuration.Release;
+    readonly string Configuration = "Release";
 
     [Parameter("Version - Optional, set to latest tag if omitted (default: null)")]
     readonly string SemVer;

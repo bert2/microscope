7578e7a3062a174a1ecdc77506a2aa972edcfbe8
bert2 <shuairan@gmail.com>
2020-11-01 11:57:13 +0100

    use string interpolation

--- src/Shared/RemoteConnection.cs
+++ src/Shared/RemoteConnection.cs
@@ -5,7 +5,7 @@ namespace Microscope.Shared {
 
     public static class PipeName {
         // Pipe needs to be scoped by PID so multiple VS instances don't compete for connecting CodeLenses.
-        public static string Get(int pid) => string.Format(@"microscope\{0}", pid);
+        public static string Get(int pid) => $@"microscope\{pid}";
     }
 
     public interface IRemoteCodeLens {
